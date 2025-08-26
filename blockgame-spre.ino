e126ae166d9bcd1d37083a31c094e27876985116
Takeshi Itagaki <bamledge.0104@gmail.com>
2025-08-27 00:06:07 +0900

    Add information bar at the bottom
    Enabled to change player color.
    Also refactored.

--- blockgame-spre.ino
+++ blockgame-spre.ino
@@ -18,16 +18,40 @@
 #define BEEPLEVEL -60
 #define BEEPLENGTH_MS 50
 
-//const int notes[8] = {262, 294, 330, 349, 392, 440, 494, 523};
-const int notes[8] = {524, 588, 660, 698, 784, 880, 988, 1046};
+const int winNotes[8] = {524, 588, 660, 698, 784, 880, 988, 1046};
+const int startNotes[8] = {988, 880, 784, 698, 784, 880, 988, 1046};
 
 unsigned long start, now, interval, milliseconds, seconds, minutes, lastTap;
 bool measStarted, measEnded, initialized;
 const unsigned long TAP_GUARD_TIME_MS = 250;
 int currentPlayer;
-int playerColor[2] = {TFT_WHITE, TFT_RED};
+
+const int colorTable[8] = {TFT_WHITE, TFT_YELLOW, TFT_BLUE, TFT_GREEN, TFT_RED, TFT_CYAN, TFT_MAGENTA, TFT_ORANGE};
+int playerColorIdx[2] = {0, 4};
+int playerColor[2];
 bool placed[2][GRID_X][GRID_Y];
-bool finished;
+bool started, finished;
+int winCount[2];
+
+enum {
+  BUTTON_NONE,
+  BUTTON_RESET,
+  BUTTON_START,
+  BUTTON_P0,
+  BUTTON_P1,  
+};
+
+struct AlignedInfo {
+  int player;
+  int xidx1;
+  int yidx1;
+  int xidx2;
+  int yidx2;
+  int xidx3;
+  int yidx3;
+  int xidx4;
+  int yidx4;
+} alignedInfo = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 SDClass theSD;
 AudioClass *theAudio;
@@ -80,37 +104,36 @@ bool isPlaced(int player, int xidx, int yidx) {
   return (xidx >= 0) && (yidx >= 0) && (xidx < GRID_X) && (yidx < GRID_Y) && placed[player][xidx][yidx];
 }
 
-void highlightAlignment(int xidx1, int yidx1, int xidx2, int yidx2, int xidx3, int yidx3, int xidx4, int yidx4, int player) {
-  fillBox(idxToQpos(xidx1), idxToQpos(yidx1), playerColor[player]);
-  fillBox(idxToQpos(xidx2), idxToQpos(yidx2), playerColor[player]);
-  fillBox(idxToQpos(xidx3), idxToQpos(yidx3), playerColor[player]);
-  fillBox(idxToQpos(xidx4), idxToQpos(yidx4), playerColor[player]);
+void highlightAlignment(struct AlignedInfo &info) {
+  fillBox(idxToQpos(info.xidx1), idxToQpos(info.yidx1), playerColor[info.player]);
+  fillBox(idxToQpos(info.xidx2), idxToQpos(info.yidx2), playerColor[info.player]);
+  fillBox(idxToQpos(info.xidx3), idxToQpos(info.yidx3), playerColor[info.player]);
+  fillBox(idxToQpos(info.xidx4), idxToQpos(info.yidx4), playerColor[info.player]);
 }
-
 bool judgeHorizontal(int player, int xidx, int yidx) {
   // Now placed[xidx][yidx] is true
   bool aligned = false;
 
-  if ((xidx - 1 >= 0) && placed[player][xidx - 1][yidx]) {
-    if ((xidx - 2 >= 0) && placed[player][xidx - 2][yidx]) {
-      if ((xidx - 3 >= 0) && placed[player][xidx - 3][yidx]) {
+  if (isPlaced(player, xidx - 1, yidx)) {
+    if (isPlaced(player, xidx - 2, yidx)) {
+      if (isPlaced(player, xidx - 3, yidx)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx, xidx - 2, yidx, xidx - 3, yidx, player);
-      } else if ((xidx + 1 < GRID_X) && placed[player][xidx + 1][yidx]) {
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx, xidx - 2, yidx, xidx - 3, yidx};
+      } else if (isPlaced(player, xidx + 1, yidx)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx, xidx - 2, yidx, xidx + 3, yidx, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx, xidx - 2, yidx, xidx + 3, yidx};
       }
-    } else if ((xidx + 1 < GRID_X) && placed[player][xidx + 1][yidx]) {
-      if ((xidx + 2 < GRID_X) && placed[player][xidx + 2][yidx]) {
+    } else if (isPlaced(player, xidx + 1, yidx)) {
+      if (isPlaced(player, xidx + 2, yidx)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx, xidx + 1, yidx, xidx + 2, yidx, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx, xidx + 1, yidx, xidx + 2, yidx};
       }
     }
-  } else if ((xidx + 1 < GRID_X) && placed[player][xidx + 1][yidx]) {
-    if ((xidx + 2 < GRID_X) && placed[player][xidx + 2][yidx]) {
-      if ((xidx + 3 < GRID_X) && placed[player][xidx + 3][yidx]) {
+  } else if (isPlaced(player, xidx + 1, yidx)) {
+    if (isPlaced(player, xidx + 2, yidx)) {
+      if (isPlaced(player, xidx + 3, yidx)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx + 1, yidx, xidx + 2, yidx, xidx + 3, yidx, player);
+        alignedInfo = {player, xidx, yidx, xidx + 1, yidx, xidx + 2, yidx, xidx + 3, yidx};
       }
     }
   }
@@ -124,11 +147,11 @@ bool judgeVertical(int player, int xidx, int yidx) {
   // Now placed[xidx][yidx] is true
   bool aligned = false;
 
-  if ((yidx + 1 < GRID_Y) && placed[player][xidx][yidx + 1]) {
-    if ((yidx + 2 < GRID_Y) && placed[player][xidx][yidx + 2]) {
-      if ((yidx + 3 < GRID_Y) && placed[player][xidx][yidx + 3]) {
+  if (isPlaced(player, xidx, yidx + 1)) {
+    if (isPlaced(player, xidx, yidx + 2)) {
+      if (isPlaced(player, xidx, yidx + 3)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx, yidx + 1, xidx, yidx + 2, xidx, yidx + 3, player);
+        alignedInfo = {player, xidx, yidx, xidx, yidx + 1, xidx, yidx + 2, xidx, yidx + 3};
       }
     }
   }
@@ -142,50 +165,50 @@ bool judgeDiagonal(int player, int xidx, int yidx) {
   // Now placed[xidx][yidx] is true
   bool aligned = false;
 
-  if ((xidx - 1 >= 0) && (yidx - 1 >= 0) && placed[player][xidx - 1][yidx - 1]) {
-    if ((xidx - 2 >= 0) && (yidx - 2 >= 0) && placed[player][xidx - 2][yidx - 2]) {
-      if ((xidx - 3 >= 0) && (yidx - 3 >= 0) && placed[player][xidx - 3][yidx - 3]) {
+  if (isPlaced(player, xidx - 1, yidx - 1)) {
+    if (isPlaced(player, xidx - 2, yidx - 2)) {
+      if (isPlaced(player, xidx - 3, yidx - 3)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx - 1, xidx - 2, yidx - 2, xidx - 3, yidx - 3, player);
-      } else if ((xidx + 1 < GRID_X) && (yidx + 1 < GRID_Y) && placed[player][xidx + 1][yidx + 1]) {
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx - 1, xidx - 2, yidx - 2, xidx - 3, yidx - 3};
+      } else if (isPlaced(player, xidx + 1, yidx + 1)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx - 1, xidx - 2, yidx - 2, xidx + 1, yidx + 1, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx - 1, xidx - 2, yidx - 2, xidx + 1, yidx + 1};
       }
-    } else if ((xidx + 1 < GRID_X) && (yidx + 1 < GRID_Y)  && placed[player][xidx + 1][yidx + 1]) {
-      if ((xidx + 2 < GRID_X) && (yidx + 2 < GRID_Y) && placed[player][xidx + 2][yidx + 2]) {
+    } else if (isPlaced(player, xidx + 1, yidx + 1)) {
+      if (isPlaced(player, xidx + 2, yidx + 2)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx - 1, xidx + 1, yidx + 1, xidx + 2, yidx + 2, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx - 1, xidx + 1, yidx + 1, xidx + 2, yidx + 2};
       }
     }
-  } else if ((xidx + 1 < GRID_X) && (yidx + 1 < GRID_Y) && placed[player][xidx + 1][yidx + 1]) {
-    if ((xidx + 2 < GRID_X) && (yidx + 2 < GRID_Y)  && placed[player][xidx + 2][yidx + 2]) {
-      if ((xidx + 3 < GRID_X) && (yidx + 3 < GRID_Y)  && placed[player][xidx + 3][yidx + 3]) {
+  } else if (isPlaced(player, xidx + 1, yidx + 1)) {
+    if (isPlaced(player, xidx + 2, yidx + 2)) {
+      if (isPlaced(player, xidx + 3, yidx + 3)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx + 1, yidx + 1, xidx + 2, yidx + 2, xidx + 3, yidx + 3, player);
+        alignedInfo = {player, xidx, yidx, xidx + 1, yidx + 1, xidx + 2, yidx + 2, xidx + 3, yidx + 3};
       }
     }
   }
 
-  if ((xidx - 1 >= 0) && (yidx + 1 < GRID_Y) && placed[player][xidx - 1][yidx + 1]) {
-    if ((xidx - 2 >= 0) && (yidx + 2 < GRID_Y) && placed[player][xidx - 2][yidx + 2]) {
-      if ((xidx - 3 >= 0) && (yidx + 3 < GRID_Y) && placed[player][xidx - 3][yidx] + 3) {
+  if (isPlaced(player, xidx - 1, yidx + 1)) {
+    if (isPlaced(player, xidx - 2, yidx + 2)) {
+      if (isPlaced(player, xidx - 3, yidx + 3)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx + 1, xidx - 2, yidx + 2, xidx - 3, yidx + 3, player);
-      } else if ((xidx + 1 < GRID_X) &&  (yidx - 1 >= 0) && placed[player][xidx + 1][yidx - 1]) {
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx + 1, xidx - 2, yidx + 2, xidx - 3, yidx + 3};
+      } else if (isPlaced(player, xidx + 1, yidx - 1)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx + 1, xidx - 2, yidx + 2, xidx + 1, yidx - 1, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx + 1, xidx - 2, yidx + 2, xidx + 1, yidx - 1};
       }
-    } else if ((xidx + 1 < GRID_X) && (yidx - 1 >= 0)  && placed[player][xidx + 1][yidx - 1]) {
-      if ((xidx + 2 < GRID_X) && (yidx - 2 >= 0) && placed[player][xidx + 2][yidx - 2]) {
+    } else if (isPlaced(player, xidx + 1, yidx - 1)) {
+      if (isPlaced(player, xidx + 2, yidx - 2)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx - 1, yidx + 1, xidx + 1, yidx - 1, xidx + 2, yidx - 2, player);
+        alignedInfo = {player, xidx, yidx, xidx - 1, yidx + 1, xidx + 1, yidx - 1, xidx + 2, yidx - 2};
       }
     }
-  } else if ((xidx + 1 < GRID_X) && (yidx - 1 >= 0) && placed[player][xidx + 1][yidx - 1]) {
-    if ((xidx + 2 < GRID_X) && (yidx - 2 >= 0)  && placed[player][xidx + 2][yidx - 2]) {
-      if ((xidx + 3 < GRID_X) && (yidx - 3 >= 0)  && placed[player][xidx + 3][yidx - 3]) {
+  } else if (isPlaced(player, xidx + 1, yidx - 1)) {
+    if (isPlaced(player, xidx + 2, yidx - 2)) {
+      if (isPlaced(player, xidx + 3, yidx - 3)) {
         aligned = true;
-        highlightAlignment(xidx, yidx, xidx + 1, yidx - 1, xidx + 2, yidx - 2, xidx + 3, yidx - 3, player);
+        alignedInfo = {player, xidx, yidx, xidx + 1, yidx - 1, xidx + 2, yidx - 2, xidx + 3, yidx - 3};
       }
     }
   }
@@ -196,9 +219,25 @@ bool judgeDiagonal(int player, int xidx, int yidx) {
   return aligned;
 }
 
+void updateBottomInfo(int highlightedPlayer = -1) {
+  spr.setTextSize(1);
+  spr.fillRect(0, 221, 320, 19, TFT_BLACK);
+  spr.drawRoundRect( 2, 222, 36, 16, 2, TFT_WHITE);
+  spr.drawRoundRect(42, 222, 36, 16, 2, TFT_WHITE);
+  spr.setCursor( 5,227);spr.setTextColor(TFT_WHITE);spr.printf("Reset");
+  spr.setCursor(45,227);spr.setTextColor(TFT_WHITE);spr.printf("Start");
+  spr.setCursor(120,227);spr.setTextColor(TFT_WHITE);spr.printf("WINs->");
+  spr.setCursor(160,227);spr.setTextColor(playerColor[0]);spr.printf(" Player1:%3d", winCount[0]);
+  spr.setCursor(240,227);spr.setTextColor(playerColor[1]);spr.printf(" Player2:%3d", winCount[1]);
+  if (highlightedPlayer == 0) {
+    spr.fillRect(160, 221, 80, 19, playerColor[0]);spr.setCursor(160,227);spr.setTextColor(TFT_BLACK);spr.printf(" Player1:%3d", winCount[0]);
+  } else if (highlightedPlayer == 1) {
+    spr.fillRect(240, 221, 80, 19, playerColor[1]);spr.setCursor(240,227);spr.setTextColor(TFT_BLACK);spr.printf(" Player2:%3d", winCount[1]);
+  }
+}
+
 void gameInit() {
   currentPlayer = 0;
-  finished = false;
   for (int p=0; p<2; p++) {
     for (int i=0; i<GRID_X; i++) {
       for (int j=0; j<GRID_Y; j++) {
@@ -206,8 +245,12 @@ void gameInit() {
       }
     }
   }
+  playerColor[0] = colorTable[playerColorIdx[0]];
+  playerColor[1] = colorTable[playerColorIdx[1]];
+  alignedInfo = {0, 0, 0, 0, 0, 0, 0, 0, 0};
   spr.fillSprite(TFT_BLACK);
   spr.drawLine(0, 220, 320, 220, TFT_DARKGRAY);
+  updateBottomInfo();
 }
 
 void placeSound() {
@@ -218,9 +261,17 @@ void placeSound() {
   theAudio->setBeep(0, 0, 0);
 }
 
+void startSound() {
+  for (int i=0; i< 8; i++) {
+    theAudio->setBeep(1, BEEPLEVEL, startNotes[i]);
+    usleep(BEEPLENGTH_MS * 1000);
+  }
+  theAudio->setBeep(0, 0, 0);
+}
+
 void winSound() {
   for (int i=0; i< 8; i++) {
-    theAudio->setBeep(1, BEEPLEVEL, notes[i]);
+    theAudio->setBeep(1, BEEPLEVEL, winNotes[i]);
     usleep(BEEPLENGTH_MS * 1000);
   }
   theAudio->setBeep(0, 0, 0);
@@ -245,6 +296,38 @@ void setup(void) {
     Serial.printf("ERROR: malloc error (tmpspr:%dByte)\n", sz);
     rtn = -1;
   }
+  winCount[0] = 0;
+  winCount[1] = 0;
+  gameInit();
+  finished = true;
+}
+
+int getTouchedInfoArea(int xidx, int yidx) {
+  if (xidx >= 0 && xidx <= 1 && yidx == 11) {
+    return BUTTON_RESET;
+  }
+  if (xidx >= 2 && xidx <= 3 && yidx == 11) {
+    return BUTTON_START;
+  }
+  if (xidx >= 8 && xidx <= 11 && yidx == 11) {
+    return BUTTON_P0;
+  }
+  if (xidx >= 12 && xidx <= 15 && yidx == 11) {
+    return BUTTON_P1;
+  }
+  return BUTTON_NONE;
+}
+
+void changePlayerColor(int player) {
+  playerColorIdx[player] = (playerColorIdx[player] + 1) % 8;
+  playerColor[player] = colorTable[playerColorIdx[player]];
+  updateBottomInfo();
+}
+
+void allReset() {
+  winCount[0] = 0;
+  winCount[1] = 0;
+  updateBottomInfo();
   gameInit();
 }
 
@@ -258,7 +341,26 @@ void loop(void) {
     now = millis();
     if (now - lastTap > TAP_GUARD_TIME_MS) {
       if (finished) {
-        gameInit();
+        qx = quantizePos(tx);
+        xidx = qx / BOX_SIZE;
+        qy = quantizePos(ty);
+        yidx = qy / BOX_SIZE;
+        int button = getTouchedInfoArea(xidx, yidx);
+        Serial.printf("button =%d, xidx=%d, yidx=%d \n", button, xidx, yidx);
+        if (button == BUTTON_RESET) {
+          allReset();
+        }
+        if (button == BUTTON_P0) {
+          changePlayerColor(0);
+        }
+        if (button == BUTTON_P1) {
+          changePlayerColor(1);
+        }
+        if (button == BUTTON_START) {
+          gameInit();
+          startSound();
+          finished = false;
+        }
       } else {
         qx = quantizePos(tx);
         xidx = min(qx / BOX_SIZE, GRID_X - 1);
@@ -272,11 +374,13 @@ void loop(void) {
           placeSound();
           //Serial.printf("Placed. Xidx=%d, Yidx=%d\n", xidx, yidx);
           if (judgeHorizontal(currentPlayer, xidx, yidx) || judgeVertical(currentPlayer, xidx, yidx) || judgeDiagonal(currentPlayer, xidx, yidx)) {
-            Serial.printf("Player %d win!\n", currentPlayer + 1);
-            finished = true;
-          }
-          if(finished){
+            winCount[currentPlayer]++;
+            Serial.printf("Player %d win! count = %d\n", currentPlayer + 1, winCount[currentPlayer]);
+            updateBottomInfo(currentPlayer);
+            highlightAlignment(alignedInfo);
+            spr.pushSprite(&lcd, 0, 0);
             winSound();
+            finished = true;
           }
           changePlayer();
         }
